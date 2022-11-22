27d98b39f6e05e6206d54e96de808ff86ceba8ea
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2022-11-22 21:55:04 +0000

    Modelling Embedding

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,6 +1,8 @@
+const { permittedCrossDomainPolicies } = require('helmet');
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
+const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -100,6 +102,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -114,6 +117,12 @@ tourSchema.virtual('durationWeeks').get(function () {
 //DOCUMENT MIDDLEWARE: runs before .save() and .create()
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async id => await User.findById);
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 
