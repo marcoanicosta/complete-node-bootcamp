81ced53ce41f207dc548b0891e4ca4eafc67cef2
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2022-11-26 23:35:29 +0000

    Project structure + blocks

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -12,6 +12,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
@@ -74,14 +75,7 @@ app.use((req, res, next) => {
   next();
 });
 
-//3) ROUTES
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forrest Hiker',
-    user: 'Jona'
-  });
-});
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
