b9182df0150e4eb7a1345c50df2539d728afd32d
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2022-12-08 23:20:27 +0000

    pre console log remove/dev

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -7,6 +7,7 @@ const mongoSanitise = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -88,6 +89,8 @@ app.use(
     ],
   })
 );
+
+app.use(compression());
 
 //Test Middleware
 app.use((req, res, next) => {
