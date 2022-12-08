19e1f783e6c5e64afb44f88897b20bd472ce4d67
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2022-12-08 23:11:46 +0000

    finsihing the api

--- 4-natours/starter/controllers/bookingController.js
+++ 4-natours/starter/controllers/bookingController.js
@@ -2,14 +2,13 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../models/tourModel');
 const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
 const factory = require('./handlerFactory');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get the currently booked tour
   const tour = await Tour.findById(req.params.tourID);
 
-  //2) Create thec checkout session
+  // 2) Create the checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
     mode: 'payment',
@@ -35,14 +34,15 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     ],
   });
 
-  //3) Create sessions as response
+  // 3) Create session as response
   res.status(200).json({
     status: 'success',
-    session,
+    session
   });
 });
 
 exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  // This is only TEMPORARY, because it's UNSECURE: everyone can make bookings without paying
   const { tour, user, price } = req.query;
 
   if (!tour && !user && !price) return next();
@@ -50,3 +50,9 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
 
   res.redirect(req.originalUrl.split('?')[0]);
 });
+
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
